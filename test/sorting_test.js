0c8c29c1f0e9775a8083b1441e7babcb1167f4a2
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-03-10 05:13:25 -0600

    working on test

--- test/sorting_test.js
+++ test/sorting_test.js
@@ -1,1 +1,10 @@
 //testing for sorting
+var assert = require('chai').assert;
+var Sort = require('../lib/sorting_suite');
+
+describe('Sort', function() {
+
+  it('should be a function', function() {
+    assert.isFunction(Sort);
+  });
+});

45dfc828ec3db545ee15a15d53cfd5b9b17777ce
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-03-10 05:49:11 -0600

    having module issues

--- test/sorting_test.js
+++ test/sorting_test.js
@@ -16,7 +16,7 @@ describe('Sort', function() {
   it('should sort an array of numbers', function() {
     var sort = new Sort();
     var a = [33, 103, 2, 726, 205, 984, 198, 764, 10];
-    var actual = sort.bubble(a)
+    var actual = sort.bubble_sort(a)
     var expected = [2, 10, 33, 103, 198, 205, 726, 764, 984]
     assert.deepEqual(actual, expected);
   });
